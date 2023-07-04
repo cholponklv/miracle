cbd77ffc774c21b57d396218fbfd44ff0d6b5df7
cholponklv <cbaiel@mail.ru>
2023-07-04 21:44:52 +0600

    text2

--- miracle/settings.py
+++ miracle/settings.py
@@ -83,12 +83,12 @@ WSGI_APPLICATION = 'miracle.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # DATABASES = {
 #     'default':{
@@ -102,9 +102,9 @@ WSGI_APPLICATION = 'miracle.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': env.db()
-}
+# DATABASES = {
+#     'default': env.db()
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
